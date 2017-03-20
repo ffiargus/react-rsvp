d60aca3c002fb8bb992771c715d1a03f142bb136
alfredya <a.yang@utoronto.ca>
2017-03-19 20:37:33 -0400

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render () {
+    return (
+      <div style={{ height: '100%' }}>
+        <h1>Training Seed Project is Running!</h1>
+      </div>
+    )
+  }
+}
+
---
+++ src/main.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+// ========================================================
+// Store Instantiation
+// ========================================================
+
+
+// ========================================================
+// Render Setup
+// ========================================================
+const MOUNT_NODE = document.getElementById('root')
+
+let render = (Component) => {
+  ReactDOM.render(<Component/>, MOUNT_NODE);
+};
+
+// This code is excluded from production bundle
+if (__DEV__) {
+  if (module.hot) {
+    // Development render functions
+    const renderApp = render
+    const renderError = (error) => {
+      const RedBox = require('redbox-react').default
+      ReactDOM.render(<RedBox error={error} />, MOUNT_NODE)
+    }
+
+    // Wrap render in try/catch
+    render = (Component) => {
+      try {
+        renderApp(Component)
+      } catch (error) {
+        console.error(error)
+        renderError(error)
+      }
+    }
+
+    // Setup hot module replacement
+    module.hot.accept('./components/App', () =>
+      setImmediate(() => {
+        const NextApp = require('./components/App').default
+        ReactDOM.unmountComponentAtNode(MOUNT_NODE)
+        render(NextApp)
+      })
+    )
+  }
+}
+
+// ========================================================
+// Go!
+// ========================================================
+render(App)
+
